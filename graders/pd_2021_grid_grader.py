c6dc64fef45ddfbe7e89f0d2f3bd05244ef90965
Benda Xu <heroxbd@gentoo.org>
2021-09-08 14:26:32 +0800

    Fix comment
    Signed-off-by: Benda Xu <heroxbd@gentoo.org>

--- graders/pd_2021_grid_grader.py
+++ graders/pd_2021_grid_grader.py
@@ -22,7 +22,7 @@ def angleScore(df_ans, df_sub):
                         np.cos(-df_ans[:, 1]+np.pi/2)
     vecproduct[vecproduct > 1] = 1
     vecproduct[vecproduct < -1] = -1
-    # vecproduct is (2000, ) , so result is [0, 2000]
+    # vecproduct is (1000, ) , so result is [0, 1000]
     return np.sum(np.arccos(vecproduct)) / np.pi
 
 
